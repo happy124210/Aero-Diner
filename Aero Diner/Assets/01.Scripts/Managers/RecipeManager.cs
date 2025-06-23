a25eeeb7502c0d03d4af2443a4971ab279f9a4f9
GUYoof <gom.nyusuk@gmail.com>
2025-06-24 00:11:25 +0900

    ✨[Feat] : 레시피매니저 구현
    ✨[Feat] : 레시피매니저 구현
    싱글톤 사용, RecipeName 스크립트 활용

---
+++ Aero Diner/Assets/01.Scripts/Managers/RecipeManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+/// <summary>
+/// 모든 레시피 데이터를 관리하고
+/// 조리가 가능한 레시피를 판단하는 매니저
+/// </summary>
+public class RecipeManager : Singleton<RecipeManager>
+{
+    [Header("모든 레시피 데이터 그룹")]
+    public RecipeDataSOGroup[] allRecipes;
+
+    /// <summary>
+    /// 해당 스테이션에서 현재 제공된 재료로 만들 수 있는 레시피들을 반환
+    /// </summary>
+    public List<RecipeData> GetCookableRecipes(CookingStation station, List<string> availableIngredients)
+    {
+        List<RecipeData> cookableRecipes = new List<RecipeData>();
+
+        foreach (var group in allRecipes)
+        {
+            foreach (var recipe in group.Recipes)
+            {
+                // 1. 스테이션이 일치하거나, None일 경우 허용
+                if (recipe.requireStation != CookingStation.None && recipe.requireStation != station)
+                    continue;
+
+                // 2. 모든 재료가 사용 가능한 재료 목록에 존재하는지 확인
+                if (recipe.ingredients.All(ingredient => availableIngredients.Contains(ingredient)))
+                {
+                    cookableRecipes.Add(recipe);
+                }
+            }
+        }
+
+        return cookableRecipes;
+    }
+
+    /// <summary>
+    /// 현재 스테이션 타입과 등록된 재료를 바탕으로
+    /// 가능한 레시피 목록을 판단하고 첫 번째 레시피를 반환
+    /// </summary>
+    public RecipeData TrySetRecipe(CookingStation station, List<string> ingredients)
+    {
+        var recipes = GetCookableRecipes(station, ingredients);
+        return recipes.FirstOrDefault(); // 가능하다면 첫 번째 레시피 반환, 없으면 null
+    }
+}
---
+++ Aero Diner/Assets/03.Datas/Recipe/RecipeDataSOGroup.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+[CreateAssetMenu(fileName = "New Recipe Group", menuName = "Game Data/Recipe Data SO Group")]
+public class RecipeDataSOGroup : ScriptableObject
+{
+    [System.Serializable]
+    public class Entry
+    {
+        public RecipeData recipeData; // 여기서 RecipeData로 캐스팅할 수 있도록!
+    }
+
+    public List<Entry> entries; // <- 실제 데이터 리스트
+
+    /// <summary>
+    /// 간편하게 레시피 리스트로 접근할 수 있도록 헬퍼 프로퍼티
+    /// </summary>
+    public List<RecipeData> Recipes => entries
+        .Where(e => e.recipeData != null)
+        .Select(e => e.recipeData)
+        .ToList();
+}
