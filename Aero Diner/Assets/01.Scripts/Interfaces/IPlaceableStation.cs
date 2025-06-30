dc0fc6cd0f99bf033ed05c70d3141dc1b7b81a9b
고윤아 <0205ria@gmail.com>
2025-06-30 22:23:52 +0900

    ✨[Update] IPlaceable 구현 추가, 테이블 배치 가능

--- Aero Diner/Assets/01.Scripts/Interfaces/IPlaceableStation.cs
+++ Aero Diner/Assets/01.Scripts/Interfaces/IPlaceableStation.cs
@@ -8,5 +8,6 @@ public interface IPlaceableStation
     /// 재료가 스테이션에 내려놓였을 때 호출되는 함수.
     /// </summary>
     /// <param name="data">내려놓는 재료 데이터</param>
-    void PlaceIngredient(ScriptableObject data);
+    void PlaceObject(ScriptableObject data);
+    void OnPlayerPickup();
 }
