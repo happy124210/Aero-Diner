268be3e1d4fceaf2a64d357f7f25f7e9e109ce86
고윤아 <0205ria@gmail.com>
2025-06-30 11:22:32 +0900

    🚚[Move] 안 쓰는 파일 정리
    * Selector, Sequence 파일 삭제
    * BT 관련 파일들 상태 패턴으로 수정

--- Aero Diner/Assets/01.Scripts/Customer/States.cs
+++ Aero Diner/Assets/01.Scripts/Customer/States.cs
@@ -1,13 +1,5 @@
 using UnityEngine;
 
-public abstract class CustomerState
-{
-    public abstract void Enter(CustomerController customer);
-    public abstract CustomerState Update(CustomerController customer);
-    public abstract void Exit(CustomerController customer);
-    public abstract string StateName { get; }
-}
-
 /// <summary>
 /// 입구로 이동
 /// </summary>
