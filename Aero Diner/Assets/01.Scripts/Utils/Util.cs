5052c2e74dd062a44b80897d429d0534c6ebfd75
고윤아 <0205ria@gmail.com>
2025-07-01 17:49:49 +0900

    ✨[Feat] 인스펙터용 ReadOnly 유틸 추가

--- Aero Diner/Assets/01.Scripts/Utils/Util.cs
+++ Aero Diner/Assets/01.Scripts/Utils/Util.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq;
 using UnityEngine;
+using UnityEditor;
 
 public static class Util
 {
@@ -38,3 +39,22 @@ public static class Util
             .Select(word => char.ToUpper(word[0]) + word.Substring(1)));
     }
 }
+
+/// <summary>
+/// ReadOnlyAttribute
+/// </summary>
+public class ReadOnlyAttribute : PropertyAttribute { }
+
+#if UNITY_EDITOR
+
+[CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+public class ReadOnlyDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        GUI.enabled = false;
+        EditorGUI.PropertyField(position, property, label, true);
+        GUI.enabled = true;
+    }
+}
+#endif
