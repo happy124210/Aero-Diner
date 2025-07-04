b9a90d919b692cc418a553b90590cbb7f6e523ab
GUYoof <gom.nyusuk@gmail.com>
2025-07-04 17:53:24 +0900

    ✨[Feat] : 레시피 선택 시 눈으로 확인 가능하게 코드 추가
    ✨[Feat] : 레시피 선택 시 눈으로 확인 가능하게 코드 추가
    
    * 스테이션 에디터 생성
    * 재료를 설비에 넣고 레시피 확인 버튼을 누르면 레시피 확인 가능

--- Aero Diner/Assets/03.Datas/Food/FoodData.cs
+++ Aero Diner/Assets/03.Datas/Food/FoodData.cs
@@ -23,13 +23,13 @@ public enum FoodType
 public class FoodData : ScriptableObject
 {
     [Header("음식 정보")]
-    public string id; // 고유 아이디, 레시피(ingredients)에 사용 (ex. f1)
+    public string id; // 고유 아이디, 레시피(ingredients)에 사용
     public string foodName;
     public string displayName; // UI용 한글 이름
-    public FoodType foodType; // Raw, Sauce, Topping, Menu 
+    public FoodType foodType;
     public Sprite foodIcon;
     public string description;
-    public StationType[] stationType; // 가공될 설비 (여러 개 가능)
+    public StationType[] stationType; // 가공될 설비
     public string[] ingredients; // 레시피. 원재료라면 null
     public float cookTime;
     public int foodCost;
