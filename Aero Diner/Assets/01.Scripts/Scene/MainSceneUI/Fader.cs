8a5a697dc0e24a538b95460e2335ab412b1933ab
StarCandy-D2 <kdh04090@gmail.com>
2025-07-11 16:50:58 +0900

    ✨[Update] Fader프리펩 추가, 메인씬에 강제로 페이드 효과 만듦.

---
+++ Aero Diner/Assets/01.Scripts/Scene/MainSceneUI/Fader.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class Fader : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup blackPanelGroup; // 알파 조절용
+    [SerializeField] private float holdTime = 0.5f;
+    [SerializeField] private float fadeDuration = 1f;
+
+    private void Start()
+    {
+        PlayFadeIn();
+    }
+
+    public void PlayFadeIn()
+    {
+        blackPanelGroup.alpha = 1f;
+        blackPanelGroup.blocksRaycasts = true;
+        blackPanelGroup.interactable = true;
+
+        Sequence seq = DOTween.Sequence();
+        seq.AppendInterval(holdTime); // 3초 유지
+        seq.Append(blackPanelGroup.DOFade(0f, fadeDuration)); // 1초 페이드
+        seq.OnComplete(() =>
+        {
+            blackPanelGroup.blocksRaycasts = false;
+            blackPanelGroup.interactable = false;
+        });
+    }
+}
