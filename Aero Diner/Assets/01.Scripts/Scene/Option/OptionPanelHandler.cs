4bb5cf68c99a148d1509895799db953e448391cc
StarCandy-D2 <kdh04090@gmail.com>
2025-07-16 10:48:49 +0900

    ✨[Update] 이벤트 기반 UI매니저에서 핸들러 분리
    UIHandler 매서드 분리하여 각 UI 전담 핸들러 만듦

---
+++ Aero Diner/Assets/01.Scripts/Scene/Option/OptionPanelHandler.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OptionPanelHandler : IUIEventHandler
+{
+    public bool Handle(UIEventType type, object payload)
+    {
+        switch (type)
+        {
+            case UIEventType.OpenPause:
+                UIRoot.Instance.pausePanel?.SetActive(true);
+                GameManager.Instance.PauseGame();
+                break;
+            case UIEventType.ClosePause:
+                UIRoot.Instance.pausePanel?.SetActive(false);
+                GameManager.Instance.ContinueGame();
+                break;
+            case UIEventType.OpenOption:
+                UIRoot.Instance.pausePanel.SetActive(false);
+                UIRoot.Instance.optionPanel.SetActive(true);
+                UIRoot.Instance.volumePanel.gameObject.SetActive(true);
+                break;
+            case UIEventType.CloseOption:
+                UIRoot.Instance.optionPanel.SetActive(false);
+                if (SceneManager.GetActiveScene().name != "StartScene")
+                    UIRoot.Instance.pausePanel.SetActive(true);
+                break;
+            case UIEventType.ShowSoundTab:
+                UIRoot.Instance.volumePanel.gameObject.SetActive(true);
+                UIRoot.Instance.videoPanel.gameObject.SetActive(false);
+                UIRoot.Instance.keysettingPanel.gameObject.SetActive(false);
+                break;
+            case UIEventType.ShowVideoTab:
+                UIRoot.Instance.volumePanel.gameObject.SetActive(false);
+                UIRoot.Instance.videoPanel.gameObject.SetActive(true);
+                UIRoot.Instance.keysettingPanel.gameObject.SetActive(false);
+                break;
+            case UIEventType.ShowControlTab:
+                UIRoot.Instance.volumePanel.gameObject.SetActive(false);
+                UIRoot.Instance.videoPanel.gameObject.SetActive(false);
+                UIRoot.Instance.keysettingPanel.gameObject.SetActive(true);
+                break;
+        }
+        return false;
+    }
+}
---
+++ Aero Diner/Assets/01.Scripts/Scene/StartScene/StartSceneUIHandler.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSceneUIHandler : IUIEventHandler
+{
+    private readonly List<GameObject> sceneUIs;
+    public StartSceneUIHandler(List<GameObject> sceneUIs)
+    {
+        this.sceneUIs = sceneUIs;
+    }
+    public bool Handle(UIEventType type, object payload)
+    {
+        switch (type)
+        {
+            case UIEventType.ShowStartMenuWithSave:
+                foreach (var ui in sceneUIs)
+                    ui?.GetComponentInChildren<MenuPanel4>(true)?.gameObject.SetActive(true);
+                break;
+            case UIEventType.ShowStartMenuNoSave:
+                foreach (var ui in sceneUIs)
+                    ui?.GetComponentInChildren<MenuPanel3>(true)?.gameObject.SetActive(true);
+                break;
+            case UIEventType.OnClickNewGame:
+                SaveLoadManager.DeleteSave(); // 모든 저장 삭제
+
+                // 씬 전환
+                EventBus.RaiseFadeEvent(FadeEventType.FadeOutAndLoadScene, new FadeEventPayload(alpha: 1f, duration: 1f, scene: "MainScene"));
+                break;
+            case UIEventType.LoadMainScene:
+                EventBus.RaiseFadeEvent(FadeEventType.FadeOutAndLoadScene, new FadeEventPayload(alpha: 1f, duration: 1f, scene: "MainScene"));
+                break;
+            case UIEventType.QuitGame:
+#if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+#else
+                Application.Quit();
+#endif
+                break;
+
+        }
+        return false;
+    }
+}
