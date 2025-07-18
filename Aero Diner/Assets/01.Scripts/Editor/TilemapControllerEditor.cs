aee5a10162faa4ca16bd5c5ecfe04e89b46fc7d8
GUYoof <gom.nyusuk@gmail.com>
2025-07-18 20:11:28 +0900

    ✨[Update] 그리드 에디터에 그리드 숨기는 버튼 추가
    ✨[Update] 그리드 에디터에 그리드 숨기는 버튼 추가

--- Aero Diner/Assets/01.Scripts/Editor/TilemapControllerEditor.cs
+++ Aero Diner/Assets/01.Scripts/Editor/TilemapControllerEditor.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 /// <summary>
 /// TilemapController의 커스텀 인스펙터
-/// 버튼을 눌러 셀을 보이게 할 수 있음
+/// 셀을 보이거나 숨기는 버튼을 제공
 /// </summary>
 [CustomEditor(typeof(TilemapController))]
 public class TilemapControllerEditor : Editor
@@ -15,9 +15,16 @@ public class TilemapControllerEditor : Editor
         TilemapController controller = (TilemapController)target;
 
         GUILayout.Space(10);
-        if (GUILayout.Button("셀 보이기 "))
+        GUILayout.Label("그리드 셀 표시 제어", EditorStyles.boldLabel);
+
+        if (GUILayout.Button("셀 보이기"))
         {
             controller.ShowAllCells();
+        }
+
+        if (GUILayout.Button("셀 숨기기"))
+        {
+            controller.HideAllCells();
         }
     }
 }
