7b87154a44d0bbc891400d04090628260551054b
GUYoof <gom.nyusuk@gmail.com>
2025-07-21 10:57:46 +0900

    ♻️[Refactor] 레피시 프리뷰 에디테 리팩토링
    ♻️[Refactor] 레피시 프리뷰 에디테 리팩토링

---
+++ Aero Diner/Assets/01.Scripts/Station/RecipePreviewResult.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+public class RecipePreviewResult
+{
+    public string BestMatchedRecipeText;
+    public FoodData CookedIngredient;
+    public List<string> AvailableFoodIds;
+
+    public static RecipePreviewResult None(string message)
+    {
+        return new RecipePreviewResult
+        {
+            BestMatchedRecipeText = message,
+            CookedIngredient = null,
+            AvailableFoodIds = new List<string>()
+        };
+    }
+}
