10277557d73629ebb6a1005cefd2ae9886594f0a
StarCandy-D2 <kdh04090@gmail.com>
2025-07-28 14:51:12 +0900

    ✨[Update] StationManager에서 인벤토리에 필요한매서드 추가
    1. 각 설비 Id 별로 특정 그리드셀에 몇개 있는지 확인하는 매서드
    2. 설비들의 해금과 관련된 매서드
    3. 인벤토리에서는 미해금된 설비는 Locked 프리펩으로
    해금된 설비는 보통 프리펩으로 프리펩을 다분화하여 불러옴.

--- Aero Diner/Assets/01.Scripts/UI/Inventory/StationPanel_ScrollView_Content.cs
+++ Aero Diner/Assets/01.Scripts/UI/Inventory/StationPanel_ScrollView_Content.cs
@@ -8,6 +8,9 @@ public class StationPanel_ScrollView_Content : BaseScrollViewItem
     [SerializeField] private Image stationIconImage;
     [SerializeField] private TMP_Text stationNameText;
     [SerializeField] private Button selectButton;
+    [SerializeField] private TMP_Text placedCountText;
+    [SerializeField] private TMP_Text storedCountText;
+
     private string stationcost;
 
     private StationData myData;
@@ -16,8 +19,15 @@ public class StationPanel_ScrollView_Content : BaseScrollViewItem
     public void Init(StationData data, Action<StationData> onClick)
     {
         myData = data;
+
         stationNameText.text = data.displayName;
         stationIconImage.sprite = data.stationIcon;
+
+        int placed = StationManager.Instance.GetPlacedStationCount(data.id);
+        int stored = StationManager.Instance.GetStoredStationCount(data.id);
+
+        placedCountText.text = $"배치됨: {placed}";
+        storedCountText.text = $"보관됨: {stored}";
 
         selectButton.onClick.RemoveAllListeners();
         selectButton.onClick.AddListener(() => onClick?.Invoke(myData));
