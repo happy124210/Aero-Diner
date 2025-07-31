aed0b97bd742aee77af9d67decacec6085b6717d
고윤아 <0205ria@gmail.com>
2025-07-31 21:21:16 +0900

    ✨[Update] 인벤토리 설비탭 보관/배치 개수 연결

--- Aero Diner/Assets/01.Scripts/UI/Inventory/StationPanel.cs
+++ Aero Diner/Assets/01.Scripts/UI/Inventory/StationPanel.cs
@@ -7,11 +7,16 @@ public class StationPanel : MonoBehaviour
     [SerializeField] private Image stationIconImage;
     [SerializeField] private TMP_Text stationNameText;
     [SerializeField] private TMP_Text descriptionText;
+    [SerializeField] private TMP_Text storedCountText;
+    [SerializeField] private TMP_Text placedCountText;
 
     public void SetData(StationData data)
     {
         stationIconImage.sprite = data.stationIcon;
         stationNameText.text = data.displayName;
         descriptionText.text = data.description;
+
+        storedCountText.text = StationManager.Instance.GetStationStoredCount(data.id).ToString();
+        placedCountText.text = StationManager.Instance.GetStationPlacedCount(data.id).ToString();
     }
 }
