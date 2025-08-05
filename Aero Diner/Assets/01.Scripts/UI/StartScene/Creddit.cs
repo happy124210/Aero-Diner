2fe59f6f0c24d84221805eda60ae4de768f65b02
StarCandy-D2 <kdh04090@gmail.com>
2025-08-05 17:45:31 +0900

    ✨[Feat] 크레딧

---
+++ Aero Diner/Assets/01.Scripts/UI/StartScene/Creddit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creddit : MonoBehaviour
+{
+    [SerializeField] private GameObject creddit;
+
+    public void OnCredditClicked()
+    {
+        creddit.SetActive(true);
+    }
+
+    public void OnExitClicked()
+    {
+        creddit.SetActive(false);
+    }
+}
