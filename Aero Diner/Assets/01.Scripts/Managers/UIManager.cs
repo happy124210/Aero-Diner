21d25a841a04409e98075f7c6050f494b5e702df
StarCandy-D2 <kdh04090@gmail.com>
2025-08-06 15:03:27 +0900

    Update UIManager.cs

--- Aero Diner/Assets/01.Scripts/Managers/UIManager.cs
+++ Aero Diner/Assets/01.Scripts/Managers/UIManager.cs
@@ -24,7 +24,8 @@ public class UIManager : Singleton<UIManager>
         typeof(Tu6),
         typeof(Tu7),
         typeof(Tu8),
-        typeof(DemoEnd)
+        typeof(DemoEnd),
+        typeof(IngredientWarnPopup),
 
         
         // 필요한 타입 추가 가능
